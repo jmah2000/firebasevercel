b279939667482d7bbd2c48ddcc05a42453085aa7
Jeremy Mah <jeremymah2000@hotmail.com>
2021-11-18 10:50:51 -0700

    testing1.0

---
+++ components/auth/provider/google.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+
+import {ProviderButton} from './../../ui/button/styles'
+import google from './../../../public/google.png'
+
+ 
+
+function GoogleProvider ({children, ...props}){
+    // auth with Google
+     return(
+         <ProviderButton>
+          <Image 
+          src={google} 
+          alt="google logo"
+          layout="fixed"
+          width={20}
+          height={20}
+          />
+           {children}
+             </ProviderButton>
+     )
+}
+
+export default GoogleProvider
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import firebase from '../firebase'
 import NavBar from '../components/navbar'
 import Brand from './../components/branding'
 
+import Button from './../components/ui/button'
+
+export default Home
+
 export default function Home() {
    console.log(firebase)
   return (
@@ -10,6 +14,10 @@ export default function Home() {
        <NavBar/>
        <Brand title="Home Page" tagline="directory app start"/>
 
+       <Button bgcolor="tomato" color="white">sign up with firebase</Button>
+          <Button bgcolor="blue" color="white">sign up with something</Button>
+          <Button bgcolor="orange" color="white">sign up with something</Button>
+      {/* comment */}
      </div>
      
        
