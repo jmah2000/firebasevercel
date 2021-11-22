573aefecefdde33a6be34ee7ee06ae9e6e7cff63
Jeremy Mah <jeremymah2000@hotmail.com>
2021-11-22 08:29:29 -0700

    Changed index.js

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,18 @@ import NavBar from '../components/navbar'
 import Brand from './../components/branding'
 
 import Button from './../components/ui/button'
+
 export default function Home() {
+   console.log(firebase)
   return (
-      <div>
+     <div>
+         <NavBar/>
+         <Brand title="Home Page" tagline="directory app start"/>
 
-          <Button bgcolor="tomato" color="white">sign up with firebase</Button>
-          <Button bgcolor="blue" color="white">sign up with something</Button>
-          <Button bgcolor="orange" color="white">sign up with something</Button>
-
-
-      </div>
+         <Button bgcolor="tomato" color="white">sign up with firebase</Button>
+         <Button bgcolor="blue" color="white">sign up with something</Button>
+         <Button bgcolor="orange" color="white">sign up with something</Button>
+      
+     </div>
   )
 }
-
-// export default function Home() {
-//    console.log(firebase)
-//   return (
-//      <div>
-//          <NavBar/>
-//          <Brand title="Home Page" tagline="directory app start"/>
-
-//          <Button bgcolor="tomato" color="white">sign up with firebase</Button>
-//          <Button bgcolor="blue" color="white">sign up with something</Button>
-//          <Button bgcolor="orange" color="white">sign up with something</Button>
-      
-//      </div>
-//   )
-// }
